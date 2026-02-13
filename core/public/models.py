30971f85a632cf2c90047eec659d1f55cd48e812
AlazarBekele <alazarthe70@gmail.com>
2026-02-13 20:01:53 +0300

    feat: work views

--- core/public/models.py
+++ core/public/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class IndexObjects (models.Model):
+
+    ImgName = models.CharField (max_length=55)
+    Image = models.ImageField (upload_to='Index_Image/')
+
+    def __str__(self):
+        return self.ImgName
--- core/public/views.py
+++ core/public/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def index (request):
-    return render (request, 'public/pages/index-3.html')
+
+    Hero_banner = IndexObjects.objects.all()
+    context = {
+        'hero_banner' : Hero_banner
+    }
+    return render (request, 'public/pages/index-3.html', context=context)
